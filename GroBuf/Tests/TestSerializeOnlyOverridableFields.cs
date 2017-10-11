c62f2dfa63c9dbe6e75f6f94c54b195925029c0d
Borovsky Arkady <borovskyav@list.ru>
2017-10-11 12:59:08 +0500

    Add tests on writable overridable prop

---
+++ GroBuf/Tests/TestSerializeOnlyOverridableFields.cs
@@ -0,0 +1,66 @@
+﻿using GroBuf.DataMembersExtracters;
+using NUnit.Framework;
+
+namespace GroBuf.Tests
+{
+    [TestFixture]
+    public class TestSerializeOnlyOverridableFields
+    {
+        [Test]
+        public void AllPropertiesExtractor_GetOnlyProp()
+        {
+            var serializer = new Serializer(new AllPropertiesExtractor(), null, GroBufOptions.MergeOnRead);
+            var expected = new ChildGetOnlyClass();
+            Assert.DoesNotThrow(() => serializer.Serialize(expected));
+        }
+
+        [Test]
+        public void PropertiesExtractor_GetOnlyProp()
+        {
+            var serializer = new Serializer(new PropertiesExtractor(), null, GroBufOptions.MergeOnRead);
+            var expected = new ChildGetOnlyClass();
+            Assert.DoesNotThrow(() => serializer.Serialize(expected));
+        }
+
+        [Test]
+        public void AllPropertiesExtractor_WritableProp()
+        {
+            var serializer = new Serializer(new AllPropertiesExtractor(), null, GroBufOptions.MergeOnRead);
+            var expected = new ChildWritableClass();
+            Assert.DoesNotThrow(() => serializer.Serialize(expected));
+        }
+
+        [Test]
+        public void PropertiesExtractor_WritableProp()
+        {
+            var serializer = new Serializer(new PropertiesExtractor(), null, GroBufOptions.MergeOnRead);
+            var expected = new ChildWritableClass();
+            Assert.DoesNotThrow(() => serializer.Serialize(expected));
+        }
+
+        private enum TestEnum1
+        {
+            First
+        }
+
+        private abstract class BaseGetOnlyClass
+        {
+            public virtual TestEnum1 States { get; }
+        }
+
+        private class ChildGetOnlyClass : BaseGetOnlyClass
+        {
+            public override TestEnum1 States { get; }
+        }
+
+        private abstract class BaseWritableClass
+        {
+            public virtual TestEnum1 States { get; }
+        }
+
+        private class ChildWritableClass : BaseGetOnlyClass
+        {
+            public override TestEnum1 States { get; }
+        }
+    }
+}
